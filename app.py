3a78f9791b0fd9060a28218e2a23e4724a6166e2
Flesner96 <f.lesner@wp.pl>
2025-06-05 09:21:50 +0200

    Add login system, dashboard protection and logout route

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login_input = request.form.get('username')
+        password_input = request.form.get('password')
+
+        if (login_input == os.getenv("LOGIN") and 
+            password_input == os.getenv("PASSWORD")):
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        else:
+            error = "Nieprawidłowy login lub hasło"
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+@app.route('/')
+def index():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+        return render_template('dashboard.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
